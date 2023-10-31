da22ed6b1255f68b483d749ecbd9db61f9f73b56
duy.thangkhmtk20 <duy.thangkhmtk20.hcmut.edu.vn>
2023-11-01 00:50:07 +0700

    fix Boss

--- src/game.py
+++ src/game.py
@@ -19,6 +19,7 @@ V_JUMP = -40
 GRAVITY = 5
 P_BULLET_DELAY = 10
 E_BULLET_DELAY = 50
+NUM_ENEMY = 50
 
 def rescaleSprite(surface, scale):
     w, h = surface.get_size()
@@ -394,9 +395,6 @@ class BossHealth(pygame.sprite.Sprite):
         self.image = pygame.Surface((SCREEN_WIDTH - self.width, self.height))
         self.rect = pygame.Rect(SCREEN_WIDTH - self.width, 0, self.width, self.height)
         self.hp_bar = pygame.Surface((500, self.height-10))
-        
-##        self.font = pygame.font.SysFont("Arial", height)
-##        self.color = (255, 255, 255)
         self.icon = pygame.image.load('Sprites/Monsters/enemy.png')
         self.icon = rescaleSprite(self.icon, self.height / SCREEN_HEIGHT)
     def update(self, hp):
@@ -406,130 +404,166 @@ class BossHealth(pygame.sprite.Sprite):
         self.hp_bar = pygame.Surface((boss_hp_width, self.height-10))
         self.hp_bar.fill((0, 255, 0))
         self.image.blit(self.hp_bar, [500 - boss_hp_width, 5])
+class Game:
+    def __init__(self, screen):
+        self.screen = screen
+        self.player = Player((0, 0))
+        
+        self.coin_score = Score('Sprites/Items/coin1.png', 4*0.09*SCREEN_HEIGHT, 0.09*SCREEN_HEIGHT, (0, 0.1*SCREEN_HEIGHT))
+        self.p_bullets = pygame.sprite.Group()
+        self.e_bullets = pygame.sprite.Group()
+        self.enemies = pygame.sprite.Group()
+        self.ladders = pygame.sprite.Group()
+        self.items = pygame.sprite.Group()
+        self.num_enemy = NUM_ENEMY
+        self.all_sprites = pygame.sprite.Group()
+        self.left_boundary = Boundary((-10, 0), 20, SCREEN_HEIGHT)
+        self.right_boundary = Boundary((MAX_WIDTH-10, 0), 20, SCREEN_HEIGHT)
+
+        self.all_sprites.add(self.left_boundary)
+        self.all_sprites.add(self.right_boundary)
+
+        self.p_health = PlayerHealth()
+        self.b_health = BossHealth()
+        for i in range(self.num_enemy):
+            enemy = Enemy((random.randint(0, MAX_WIDTH), random.randint(0, SCREEN_HEIGHT)))
+            self.enemies.add(enemy)
+            self.all_sprites.add(enemy)
+        for i in range(0, MAX_WIDTH, SCREEN_WIDTH):
+            for h_ratio in [0.25, 0.5, 0.75]:
+                ladder = Ladder((random.randint(i, i+SCREEN_WIDTH //2), h_ratio*SCREEN_HEIGHT), random.randint(SCREEN_WIDTH //4, SCREEN_WIDTH //2 - 50), 20, (255, 0, 0))
+                self.ladders.add(ladder)
+                self.all_sprites.add(ladder)
+        self.isBossAppeared = False
+    def update(self, pressed_keys):
+        p_bullet = self.player.fire()
+        if p_bullet:
+            self.p_bullets.add(p_bullet)
+            self.all_sprites.add(p_bullet)
+        for enemy in self.enemies.sprites():
+            e_bullet = enemy.fire()
+            if e_bullet:
+                self.e_bullets.add(e_bullet)
+                self.all_sprites.add(e_bullet)
+        self.enemies.update(player_rect = self.player.rect)
+        enemy_collide = pygame.sprite.groupcollide(self.enemies,self.p_bullets,False, True)
+        enemy_falls = pygame.sprite.groupcollide(self.enemies,self.ladders,False, False)
+        player_collide = pygame.sprite.spritecollide(self.player,self.ladders,False)
+        player_bullet = pygame.sprite.spritecollide(self.player,self.e_bullets,False)
+
+        self.player.update(pressed_keys)
+
+        if len(self.enemies.sprites()) == 0 and not self.isBossAppeared:
+            self.isBossAppeared = True
+            self.boss = Boss((SCREEN_WIDTH - 100, 0))
+            self.all_sprites.add(self.boss)
+        if self.isBossAppeared:
+            self.boss.update(player_rect = self.player.rect)
+            e_bullet = self.boss.fire()
+            if e_bullet:
+                self.e_bullets.add(e_bullet)
+                self.all_sprites.add(e_bullet)
+            boss_hurted = pygame.sprite.spritecollide(self.boss,self.p_bullets,False)
+            if boss_hurted:
+                for bullet in boss_hurted:
+                    self.boss.hurted(bullet.damage)
+                    bullet.kill()
+            self.b_health.update(self.boss.hp)
+        if player_bullet:
+            for bullet in player_bullet:
+                self.player.hurted(bullet.damage)
+                bullet.kill()
+        if player_collide:
+            for lad in player_collide:
+                self.player.fall(lad)
+        if enemy_falls:
+            for res in enemy_falls:
+                res.fall(enemy_falls[res][0])
+        if self.player.rect.left > SCREEN_WIDTH // 2 and self.right_boundary.rect.left + 10 > (SCREEN_WIDTH):
+            dx = self.player.rect.left - (SCREEN_WIDTH // 2)
+            for sprite in self.all_sprites.sprites():
+                sprite.trans_screen(dx)
+            self.player.rect.left = SCREEN_WIDTH // 2
+        elif self.player.rect.left < 10 and self.left_boundary.rect.left+10 < 0:
+            dx = self.player.rect.left - 10
+            for sprite in self.all_sprites.sprites():
+                sprite.trans_screen(dx)
+            self.player.rect.left = 10
+        if enemy_collide:
+            for res in enemy_collide:
+                item = res.hurted(enemy_collide[res][0].damage)
+                if item:
+                    self.items.add(item)
+                    self.all_sprites.add(item)
+        pitem_collide = pygame.sprite.spritecollide(self.player,self.items,False)
+        if pitem_collide:
+            for item in pitem_collide:
+                self.coin_score.score += item.score
+                self.player.upgrade(item.addbullet)
+                item.kill()
+        self.p_bullets.update()
+        self.e_bullets.update()
+        self.items.update()
+        self.p_health.update(self.player.hp)
+        self.coin_score.update()
+        self.screen.fill((0, 0, 0))
+
+
+        self.screen.blit(self.left_boundary.image, self.left_boundary.rect)
+        self.screen.blit(self.right_boundary.image, self.right_boundary.rect)
+        self.screen.blit(self.coin_score.image, self.coin_score.rect)
+        self.screen.blit(self.p_health.image, self.p_health.rect)
+        self.ladders.draw(screen)
+        if self.isBossAppeared:
+            self.screen.blit(self.b_health.image, self.b_health.rect)
+            self.screen.blit(self.boss.image, self.boss.rect)
+        screen.blit(self.player.image, self.player.rect)
+        self.items.draw(self.screen)
+        self.p_bullets.draw(self.screen)
+        self.e_bullets.draw(self.screen)
+        self.enemies.draw(self.screen)
+
+##class Menu:
+##    class NewGame(pygame.sprite.Sprite):
+##        def __init__(self):
+##            pygame.sprite.Sprite.__init__(self)
+##            self.image = pygame.Surface((1280, 800))
+##            self.rect = pygame.Rect(0, 0, self.width, self.height)
+##            self.hp_bar = pygame.Surface((500, self.height-10))
+##            self.icon = pygame.image.load('Sprites/Character/player.png')
+##            self.icon = rescaleSprite(self.icon, self.height / SCREEN_HEIGHT)
+##
+##            self.font = pygame.font.SysFont("Arial", )
+##            self.color = (255, 255, 255)
+##            self.icon = pygame.image.load(sprite_img)
+##            self.icon = rescaleSprite(self.icon, height / SCREEN_HEIGHT)
+##            self.textSurf = self.font.render('x0', 1, self.color)
+##        def update(self, hp):
+##            self.image.fill((0, 0, 0))
+##            self.image.blit(self.icon, [0, 0])
+##            self.hp_bar = pygame.Surface(((hp*5 if hp > 0 else 0), self.height-10))
+##            self.hp_bar.fill((0, 255, 0))
+##            self.image.blit(self.hp_bar, [self.icon.get_width()+1, 5])
+##    def __init__(self, screen):
+##        self.screen = screen
+##        self.screen
+
 pygame.init()
 
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 pygame.display.set_caption("ANDY'S ADVENTURE")
-player = Player((0, 0))
-running = True
 clock = pygame.time.Clock()
-coin_score = Score('Sprites/Items/coin1.png', 4*0.09*SCREEN_HEIGHT, 0.09*SCREEN_HEIGHT, (0, 0.1*SCREEN_HEIGHT))
-p_bullets = pygame.sprite.Group()
-e_bullets = pygame.sprite.Group()
-enemies = pygame.sprite.Group()
-ladders = pygame.sprite.Group()
-items = pygame.sprite.Group()
-num_enemy = 50
-all_sprites = pygame.sprite.Group()
-left_boundary = Boundary((-10, 0), 20, SCREEN_HEIGHT)
-right_boundary = Boundary((MAX_WIDTH-10, 0), 20, SCREEN_HEIGHT)
-
-all_sprites.add(left_boundary)
-all_sprites.add(right_boundary)
-
-p_health = PlayerHealth()
-b_health = BossHealth()
-for i in range(num_enemy):
-    enemy = Enemy((random.randint(0, MAX_WIDTH), random.randint(0, SCREEN_HEIGHT)))
-    enemies.add(enemy)
-    all_sprites.add(enemy)
-for i in range(0, MAX_WIDTH, SCREEN_WIDTH):
-    for h_ratio in [0.25, 0.5, 0.75]:
-        ladder = Ladder((random.randint(i, i+SCREEN_WIDTH //2), h_ratio*SCREEN_HEIGHT), random.randint(SCREEN_WIDTH //4, SCREEN_WIDTH //2 - 50), 20, (255, 0, 0))
-        ladders.add(ladder)
-        all_sprites.add(ladder)
-isBossAppeared = False
+game = Game(screen)
+##menu = Menu()
+running = True
+inGame = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pressed_keys = pygame.key.get_pressed()
-    p_bullet = player.fire()
-    if p_bullet:
-        p_bullets.add(p_bullet)
-        all_sprites.add(p_bullet)
-    for enemy in enemies.sprites():
-        e_bullet = enemy.fire()
-        if e_bullet:
-            e_bullets.add(e_bullet)
-            all_sprites.add(e_bullet)
-    enemies.update(player_rect = player.rect)
-    enemy_collide = pygame.sprite.groupcollide(enemies,p_bullets,False, True)
-    enemy_falls = pygame.sprite.groupcollide(enemies,ladders,False, False)
-    player_collide = pygame.sprite.spritecollide(player,ladders,False)
-    player_bullet = pygame.sprite.spritecollide(player,e_bullets,False)
-    player.update(pressed_keys)
-    if len(enemies.sprites()) == 0 and not isBossAppeared:
-        isBossAppeared = True
-        boss = Boss((SCREEN_WIDTH - 100, 0))
-    if isBossAppeared:
-        boss.update(player_rect = player.rect)
-        e_bullet = boss.fire()
-        if e_bullet:
-            e_bullets.add(e_bullet)
-            all_sprites.add(e_bullet)
-        boss_hurted = pygame.sprite.spritecollide(boss,p_bullets,False)
-        if boss_hurted:
-            for bullet in boss_hurted:
-                boss.hurted(bullet.damage)
-                bullet.kill()
-        b_health.update(boss.hp)
-    if player_bullet:
-        for bullet in player_bullet:
-            player.hurted(bullet.damage)
-            bullet.kill()
-    if player_collide:
-        for lad in player_collide:
-            player.fall(lad)
-    if enemy_falls:
-        for res in enemy_falls:
-            res.fall(enemy_falls[res][0])
-    if player.rect.left > SCREEN_WIDTH // 2 and right_boundary.rect.left + 10 > (SCREEN_WIDTH):
-        dx = player.rect.left - (SCREEN_WIDTH // 2)
-        for sprite in all_sprites.sprites():
-            sprite.trans_screen(dx)
-        player.rect.left = SCREEN_WIDTH // 2
-    elif player.rect.left < 10 and left_boundary.rect.left+10 < 0:
-        dx = player.rect.left - 10
-        for sprite in all_sprites.sprites():
-            sprite.trans_screen(dx)
-        player.rect.left = 10
-    if enemy_collide:
-        for res in enemy_collide:
-            item = res.hurted(enemy_collide[res][0].damage)
-            if item:
-                items.add(item)
-                all_sprites.add(item)
-    pitem_collide = pygame.sprite.spritecollide(player,items,False)
-    if pitem_collide:
-        for item in pitem_collide:
-            coin_score.score += item.score
-            player.upgrade(item.addbullet)
-            item.kill()
-    p_bullets.update()
-    e_bullets.update()
-    items.update()
-    p_health.update(player.hp)
-    coin_score.update()
-    screen.fill((0, 0, 0))
-
-
-    screen.blit(left_boundary.image, left_boundary.rect)
-    screen.blit(right_boundary.image, right_boundary.rect)
-    screen.blit(coin_score.image, coin_score.rect)
-    screen.blit(p_health.image, p_health.rect)
-    ladders.draw(screen)
-    if isBossAppeared:
-        screen.blit(b_health.image, b_health.rect)
-        screen.blit(boss.image, boss.rect)
-    screen.blit(player.image, player.rect)
-    items.draw(screen)
-    p_bullets.draw(screen)
-    e_bullets.draw(screen)
-    enemies.draw(screen)
     
+    pressed_keys = pygame.key.get_pressed()
+    game.update(pressed_keys)
     pygame.display.flip()
     clock.tick(30)
-
-# Done! Time to quit.
 pygame.quit()
